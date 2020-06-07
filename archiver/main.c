817a5785d976261765e097ab8e915843988f4303
alexgiving <alexgiving@mail.ru>
2020-06-07 21:47:54 +0300

    Initial Commit

---
+++ archiver/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  archiver
+//
+//  Created by Алексей Трутнев on 07.06.2020.
+//  Copyright © 2020 Алексей Трутнев. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
