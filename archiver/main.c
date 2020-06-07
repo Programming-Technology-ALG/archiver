82d64db6f8b1c473bc212ded3567dc9abfc84909
alexgiving <alexgiving@mail.ru>
2020-06-07 21:54:24 +0300

    packing

--- archiver/main.c
+++ archiver/main.c
@@ -1,15 +1,25 @@
 //
 //  main.c
-//  archiver
+//  archivator
 //
 //  Created by Алексей Трутнев on 07.06.2020.
 //  Copyright © 2020 Алексей Трутнев. All rights reserved.
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include "huffman.h"
+#include "locale.h"
+#include <string.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    if(argc < 4)
+        return -1;
+    unsigned char codes[SIZE][CODE_SIZE] = { 0 };
+    long bitstringLength, fileLength;
+    PackFile(argv[1], argv[2], &bitstringLength, &fileLength);
+    printf("File packed to: %s\n", argv[2]);
+    UnpackFile(argv[2], argv[3], bitstringLength, fileLength);
+    printf("File unpacked to: %s\n", argv[3]);
     return 0;
 }
